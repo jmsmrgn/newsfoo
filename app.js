ec672b1739fa5e4be69a9d27b55240785e3ddcd8
jmsmrgn <hello@jmsmrgn.com>
2015-10-29 19:12:09 -0700

    Setup data models

--- app.js
+++ app.js
@@ -4,11 +4,26 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+// var passport = require('passport');
+var app = express();
+
+// connect MongoDB
+mongoose.connect('mongodb://localhost/news', function(err,db) {
+  if (!err) {
+    console.log('Connected to /news!');
+  } else{
+    console.dir(err); //failed to connect
+  }
+});
+
+require('./models/Posts');
+require('./models/Comments');
+// require('./models/Users');
+// require('./config/passport');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -55,6 +70,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
